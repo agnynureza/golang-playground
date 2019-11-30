9071ad34be0c16f5249b5d8a54c165cf04b73937
agnynureza <agny.nureza@amarbank.co.id>
2019-11-30 14:15:20 +0700

    aggregate data mongo golang

--- noSql MongoDb/main.go
+++ noSql MongoDb/main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	update()
+	aggregateData()
 }
--- noSql MongoDb/service.go
+++ noSql MongoDb/service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -87,10 +88,71 @@ func update() {
 
 	selector := bson.M{"name": "wick"}
 	changes := student{"John Wick", 10}
-	_, err = db.Collection("stundent").UpdateOne(ctx, selector, bson.M{"$set": changes})
+	_, err = db.Collection("student").UpdateOne(ctx, selector, bson.M{"$set": changes})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Update success!")
+}
+
+func remove() {
+	db, err := connect()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	_, err = db.Collection("student").DeleteOne(ctx, bson.M{"name": "wick"})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Remove success!")
+}
+
+//aggregate data
+
+func aggregateData() {
+	pipeline := make([]bson.M, 0)
+	err := bson.UnmarshalExtJSON([]byte(strings.TrimSpace(`[
+			{
+				"$group":{
+					"_id": null,
+					"Total" :{"$sum": 1}
+				}
+			},{
+				"$project":{
+					"Total": 1,
+					"_id": 0
+				}
+			}
+		]`)), true, &pipeline)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	db, err := connect()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	csr, err := db.Collection("student").Aggregate(ctx, pipeline)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer csr.Close(ctx)
+	result := make([]bson.M, 0)
+	for csr.Next(ctx) {
+		var row bson.M
+		err := csr.Decode(&row)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		result = append(result, row)
+	}
+	if len(result) > 0 {
+		fmt.Println("Total :", result[0]["Total"])
+	}
 }
